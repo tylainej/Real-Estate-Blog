c80c00690811840ee4408f7261ec9155990d72e8
Tylaine Jeffrey <tylainej@gmail.com>
2018-09-05 13:01:58 -0400

    added some seed data

--- blog-api/real-blog/db/seeds.rb
+++ blog-api/real-blog/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create!([
+    {title: "New York Rocks", content: "I love living in NYC it rocks", subject: "Popular Cities"},
+    {title: "Chicago Sucks", content: "Never moving there, it doesn't rock", suject: "Worst US cities"},
+    {title: "LA is HOT", content: "LA has a lot of dry heat and I have never been", subject: "Best Weather to live in"}
+])
+
+Comment.create!([
+    {content: "I agree, I think the same", article_id: 1},
+    {content: "I don't agree and you suck for thinking that", article_id: 2},
+    {contnet: "I don't care you wasted your time telling the world that", article_id: 3}
+
+])
