7184d9e85fc2d13603f3b0628db0116c43ba35cd
Tylaine Jeffrey <tylainej@gmail.com>
2018-09-13 11:32:18 -0400

    added url set up for heroku

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://aboriginal-pocket.surge.sh/"
 
 export function fetchArticles(articles) {
     return fetch(`${BASE_URL}/articles`)
